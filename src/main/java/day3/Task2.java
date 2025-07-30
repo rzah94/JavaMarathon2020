01ee800bcbb25c574bf5281410f1b29e99b0d3df
rzah <rzah94@yandex.ru>
2025-07-30 06:53:13 +0300

    Add Tasks for 3 day solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите делимое и делитель: ");
+            double dividend = scanner.nextDouble();
+            double divisor = scanner.nextDouble();
+
+            while (divisor != 0) {
+                System.out.println(dividend / divisor);
+
+                System.out.print("Введите делимое и делитель: ");
+                dividend = scanner.nextDouble();
+                divisor = scanner.nextDouble();
+            }
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,26 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            for (int i = 0; i < 5; i++) {
+
+                System.out.print("Введите делимое и делитель: ");
+                double dividend = scanner.nextDouble();
+                double divisor = scanner.nextDouble();
+
+                if (divisor == 0) {
+                    System.out.println("Деление на ноль!");
+                    continue;
+                }
+
+                System.out.println(dividend / divisor);
+            }
+        }
 
     }
 }
